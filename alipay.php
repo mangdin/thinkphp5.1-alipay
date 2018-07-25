4e1ecd4b739f9c35979a9246100323816943702a
mangdin <41657311+mangdin@users.noreply.github.com>
2018-07-25 21:11:34 +0800

    Create alipay.php

---
+++ alipay.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by 合肥芒丁数据系统有限责任公司.
+ * User: wangyang
+ * Date: 2018/6/1
+ * Time: 14:04
+ */
+
+return  [
+    //支付宝即时到帐接口信息
+    'partner' =>   '',
+    'seller_email'  =>  '@sina.cn',
+    'key' => '',
+    'sign_type' => 'MD5',
+    'transport' => 'http'
+];
